9fa0fb986fc6efab64ede0cd8fad704c4fe1239f
Agnieszka Jankowska <ajankowska0404@gmail.com>
2018-05-10 11:55:57 +0200

    interface for register case

--- HackatonApp/wwwroot/js/site.js
+++ HackatonApp/wwwroot/js/site.js
@@ -1,1 +1,10 @@
-﻿// Write your JavaScript code.
+﻿// Write your Javascript code.
+document.addEventListener("DOMContentLoaded", function (event) {
+    $('#register-debtor').on('submit', function (e) {
+        e.preventDefault();
+        console.log($(this));
+        var debtorData = $(this).serialize();
+        console.log(debtorData);
+    });
+});
+
